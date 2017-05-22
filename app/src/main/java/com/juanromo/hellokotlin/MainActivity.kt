0f91c503342422052f392b5a8c539a673e5fc1a1
Juan Romo <jromo@mavs.uta.edu>
2017-05-21 20:44:35 -0500

    Snackbar added

--- app/src/main/java/com/juanromo/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/juanromo/hellokotlin/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.juanromo.hellokotlin
 
 import android.os.Bundle
+import android.support.design.widget.CoordinatorLayout
 import android.support.design.widget.FloatingActionButton
+import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val root = findViewById(R.id.root) as CoordinatorLayout
+
+        val snack = Snackbar.make(root, R.string.marked_as_read, Snackbar.LENGTH_SHORT)
+                .setAction(R.string.undo, {}) // A snack is dismissed by default when clicking the action.
+
         val fab = findViewById(R.id.fab) as FloatingActionButton
-        fab.setOnClickListener({ _ -> toast(R.string.message) })
+        fab.setOnClickListener({ snack.show() })
     }
 }
